28df6e01e1f263487089182f4f9592a0b6e0ca83
keenan137 <keenan.dup137@gmail.com>
2023-12-06 22:57:07 +0200

    Added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
 import './App.scss'
 import { Home } from './home/Home'
+import { Button } from "@/components/ui/button"
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <div className='px-6'>
             <Home />
-
+            <Button>Click me</Button>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
             </div>
         </div>
     )
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-    },
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server: {
+    port: 3000
+  }
+})
