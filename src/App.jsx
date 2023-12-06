044b01c014f0eab9dfa6740bc5b5df047266da4e
keenan137 <keenan.dup137@gmail.com>
2023-12-06 23:46:06 +0200

    Added basic routing and pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.scss'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from './components/Header'
-import { Button } from "@/components/ui/button"
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 
 function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,41 @@
-import Container from './ui/container'
+import Container from './ui/container';
+import { Button } from './ui/button';
+import { Link } from 'react-router-dom';
 
 function Header() {
+
+    const routes = [
+        {
+            href: "/",
+            label: "Home"
+        },
+        {
+            href: "/",
+            label: "About"
+        },
+    ];
+
     return (
         <header className='sm:flex sm:justify-between py-3 px-4 border-b'>
             <Container>
-                <div className='relative px-4 sm:px-6 lg:px-8 flex h-10 item-center justify-between w-full'>
+                <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 item-center justify-between w-full'>
                     <div className='flex items-center'>
                         <h1 className='ml-4 lg:ml-0 text-xl'>Full-Stack Web Developer</h1>
                     </div>
+
+                    <nav className='mx-6 flex items-center space-x-4 lg:space-x-6 hidden md:block'>
+                        {routes?.map((route, index) => {
+                            <Button asChild variant="ghost">
+                                <Link
+                                    key={index}
+                                    href={route.href}
+                                    className="text-sm font-medium transition-colors"
+                                >
+                                    {route.label}
+                                </Link>
+                            </Button>
+                        })}
+                    </nav>
                 </div>
             </Container>
         </header>
